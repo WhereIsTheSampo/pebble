fc637246f492ff4f821c9e9d37d764a2f8360df8
Bob Reid <rjreid3000@gmail.com>
2017-05-16 22:18:47 -0600

    completed part 2 of the tutorial

--- watchface/src/c/watchface.c
+++ watchface/src/c/watchface.c
@@ -2,6 +2,9 @@
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static GFont s_time_font;
+static BitmapLayer *s_background_layer;
+static GBitmap *s_background_bitmap;
 
 static void update_time()
 {
@@ -31,11 +34,25 @@ static void main_window_load(Window *window)
     // Create the TextLayer with specific bounds
     s_time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
 
+    // Crate GFont
+    s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
+
+    // Create GBitmap
+    s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
+
+    // Create BitmapLayer to display the GBitmap
+    s_background_layer = bitmap_layer_create(bounds);
+
+    // Set the bitmap onto the layer and add to the window
+    bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+    layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
+
     // Improve the layout to be more like a watchface
     text_layer_set_background_color(s_time_layer, GColorClear);
     text_layer_set_text_color(s_time_layer, GColorBlack);
     //text_layer_set_text(s_time_layer, "00:00");
-    text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+    //text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+    text_layer_set_font(s_time_layer, s_time_font);
     text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
     // add it as a child layer to the Window's root layer
@@ -46,6 +63,12 @@ static void main_window_unload(Window *window)
 {
     // Destroy TextLayer
     text_layer_destroy(s_time_layer);
+    // Unload GFont
+    fonts_unload_custom_font(s_time_font);
+    // Destroy GBitmap
+    gbitmap_destroy(s_background_bitmap);
+    // Destroy BitmapLayer
+    bitmap_layer_destroy(s_background_layer);
 }
 
 static void init() 
@@ -55,6 +78,8 @@ static void init()
 
     // Create main Window element and assign to pointer
     s_main_window = window_create();
+
+    window_set_background_color(s_main_window, GColorBlack);
 
     // Set handlers to manage the elements inside the Window
     window_set_window_handlers(s_main_window, (WindowHandlers) { .load = main_window_load, .unload = main_window_unload });
